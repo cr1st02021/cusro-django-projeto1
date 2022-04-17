05af472a51d88bf2639c4f5437d711dc977bb345
Cristiano Alves <cr1st0@hotmail.com>
2022-04-17 16:16:18 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,6 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Cristiano Alves',
+    })
