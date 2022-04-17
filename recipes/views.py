33a4a6c2bc8208b4288d3ec2979659a24653c6c7
Cristiano Alves <cr1st0@hotmail.com>
2022-04-17 10:39:44 -0300

    atualizado

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('Contato')
