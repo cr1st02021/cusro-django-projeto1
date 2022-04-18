276e57428591743e13126ed9a60b568ed87b6381
Cristiano Alves <cr1st0@hotmail.com>
2022-04-18 16:07:28 -0300

    uma nova url para uma nova view

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Cristiano Alves',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Cristiano Alves',
+    })
