6b1bd28849793a5906700b9a4377e273d8915251
Cristiano Alves <cr1st0@hotmail.com>
2022-04-19 09:27:50 -0300

    injetando dados na lista view home

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from util.recipes.factory import make_recipe
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Cristiano Alves',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Cristiano Alves',
+        'recipe': make_recipe(),
     })
